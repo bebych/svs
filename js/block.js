0afe1fa647f88c8824e2889dfa9687791123e4ba
bebych <bebych@gmail.com>
2025-07-01 21:26:48 +0300

    Update block.js

--- js/block.js
+++ js/block.js
@@ -1,95 +1,66 @@
-( function( blocks, element, components ) {
+(function(blocks, element, components, editor) {
     var el = element.createElement;
     var TextControl = components.TextControl;
-    var NumberControl = components.TextControl;
+    var InspectorControls = editor.InspectorControls;
+    var PanelBody = components.PanelBody;
 
-    blocks.registerBlockType( 'smart-video-switcher/video-block', {
+    blocks.registerBlockType('smart-video-switcher/video-block', {
         title: 'Smart Video',
         icon: 'video-alt3',
         category: 'embed',
         attributes: {
-            youtubeUrl: {
-                type: 'string',
-                default: ''
-            },
-            vkUrl: {
-                type: 'string',
-                default: ''
-            },
-            width: {
-                type: 'string',
-                default: '640'
-            },
-            height: {
-                type: 'string',
-                default: '360'
-            }
+            youtubeUrl: { type: 'string', default: '' },
+            vkUrl: { type: 'string', default: '' },
+            width: { type: 'string', default: '640' },
+            height: { type: 'string', default: '360' }
         },
         
-        edit: function( props ) {
+        edit: function(props) {
             var attributes = props.attributes;
             
-            function onChangeYoutubeUrl( newUrl ) {
-                props.setAttributes( { youtubeUrl: newUrl } );
-            }
-            
-            function onChangeVkUrl( newUrl ) {
-                props.setAttributes( { vkUrl: newUrl } );
-            }
-            
-            function onChangeWidth( newWidth ) {
-                props.setAttributes( { width: newWidth } );
-            }
-            
-            function onChangeHeight( newHeight ) {
-                props.setAttributes( { height: newHeight } );
-            }
-            
-            return el( 'div', { className: props.className },
-                el( TextControl, {
-                    label: 'YouTube URL',
-                    value: attributes.youtubeUrl,
-                    onChange: onChangeYoutubeUrl,
-                } ),
-                el( TextControl, {
-                    label: 'VK URL',
-                    value: attributes.vkUrl,
-                    onChange: onChangeVkUrl,
-                } ),
-                el( NumberControl, {
-                    label: 'Ширина',
-                    value: attributes.width,
-                    onChange: onChangeWidth,
-                    type: 'number'
-                } ),
-                el( NumberControl, {
-                    label: 'Высота',
-                    value: attributes.height,
-                    onChange: onChangeHeight,
-                    type: 'number'
-                } )
-            );
+            return [
+                el(InspectorControls, {},
+                    el(PanelBody, { title: 'Video Settings' },
+                        el(TextControl, {
+                            label: 'YouTube URL',
+                            value: attributes.youtubeUrl,
+                            onChange: (newVal) => props.setAttributes({ youtubeUrl: newVal }),
+                        }),
+                        el(TextControl, {
+                            label: 'VK URL',
+                            value: attributes.vkUrl,
+                            onChange: (newVal) => props.setAttributes({ vkUrl: newVal }),
+                        }),
+                        el(TextControl, {
+                            label: 'Width',
+                            value: attributes.width,
+                            onChange: (newVal) => props.setAttributes({ width: newVal }),
+                            type: 'number'
+                        }),
+                        el(TextControl, {
+                            label: 'Height',
+                            value: attributes.height,
+                            onChange: (newVal) => props.setAttributes({ height: newVal }),
+                            type: 'number'
+                        })
+                    )
+                ),
+                el('div', { className: props.className }, 'Smart Video Switcher Block. Enter URLs in the settings sidebar.')
+            ];
         },
         
-        save: function( props ) {
+        save: function(props) {
             var attributes = props.attributes;
-            var shortcode = '[svs_video';
+            // === ИСПРАВЛЕНИЕ 3: Замена префикса шорткода ===
+            var shortcode = '[ssvy_video';
             
-            if ( attributes.youtubeUrl ) {
-                shortcode += ' youtube_url="' + attributes.youtubeUrl + '"';
-            }
-            if ( attributes.vkUrl ) {
-                shortcode += ' vk_url="' + attributes.vkUrl + '"';
-            }
-            if ( attributes.width ) {
-                shortcode += ' width="' + attributes.width + '"';
-            }
-            if ( attributes.height ) {
-                shortcode += ' height="' + attributes.height + '"';
-            }
+            if (attributes.youtubeUrl) shortcode += ' youtube_url="' + attributes.youtubeUrl + '"';
+            if (attributes.vkUrl) shortcode += ' vk_url="' + attributes.vkUrl + '"';
+            if (attributes.width) shortcode += ' width="' + attributes.width + '"';
+            if (attributes.height) shortcode += ' height="' + attributes.height + '"';
             shortcode += ']';
             
-            return el( 'div', null, shortcode );
+            return el('div', null, shortcode);
         }
-    } );
-} )( window.wp.blocks, window.wp.element, window.wp.components ); 
+    });
+})(window.wp.blocks, window.wp.element, window.wp.components, window.wp.editor);
